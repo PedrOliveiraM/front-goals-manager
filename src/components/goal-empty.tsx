0879585dbeaa5c193321a3fa8726a903cb46482f
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-17 17:17:59 -0300

    :sparkles: create component goal-empty

---
+++ src/components/goal-empty.tsx
@@ -0,0 +1,9 @@
+const GoalEmpty = () => {
+  return (
+    <p className="text-zinc-400 text-sm leading-relaxed">
+      Você ainda não completou nenhuma meta essa semana.
+    </p>
+  )
+}
+
+export default GoalEmpty
